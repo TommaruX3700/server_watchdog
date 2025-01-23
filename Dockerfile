d6b439fca7653178e254cbdf2de153b8ccf59837
TommaruX3700 <tommasomaruzzo@gmail.com>
2025-01-23 15:58:23 +0100

    Update Dockerfile and builder.bat for release management; add release_repo file for repository configuration

--- Dockerfile
+++ Dockerfile
@@ -3,17 +3,28 @@
 FROM rust as builder
 WORKDIR /usr/src/myapp
 COPY . .
+## get the repo from the copied "release_repo" file
+RUN export REPO=$(grep repo ./release_repo | cut -d '=' -f2) && \ echo "Debian repo is at: $REPO"
+# ENV REPO=${REPO}
+
 RUN apt-get update && apt-get upgrade -y && apt-get install git
+# compile the code for debian usage, 
 RUN cargo install --path ./server_watchdog/
+RUN cargo install --path ./installer/
+RUN cargo install --path ./uninstaller/
 
-# compile the code for debian usage, clone the debian-release branche and push binaries to it
-## push here for debian binaries
+RUN git remote add origin ${REPO} 
+RUN fetch origin 
+RUN checkout -b deb_release origin/deb_release 
+RUN git add . 
+RUN git commit -m "New debian release" 
+RUN git push --set-upstream origin deb_release
 
-## this will setup a debian enviroment
+#clone the debian-release branch and push binaries to it
+
+## this will setup a debian enviroment and run the program
 # FROM debian:bookworm-slim
 # RUN apt-get update && apt-get upgrade -y && rm -rf /var/lib/apt/lists/* && apt-get install libc6 && apt-get install git
 # COPY --from=builder /usr/local/cargo/bin/server_watchdog /usr/local/bin/server_watchdog
 # RUN git checkout -b 
-
-
-CMD ["server_watchdog"]
+# CMD ["server_watchdog"]
--- builder.bat
+++ builder.bat
@@ -5,20 +5,19 @@
 ::  > Git
 
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-:: 1 - build for windows and push to release
+:: 1 - build for windows
 cargo -vV >nul 2>nul
 if %errorlevel% neq 0 (
     echo Rust is not installed or properly configured!  Shutting down . . .
     exit /b 1
 )
 
-cargo build .\server_watchdog
-cargo build .\installer
-cargo build .\unistaller
-
-setlocal enabledelayedexpansion
+cargo build --release .\server_watchdog
+cargo build --release .\installer
+cargo build --release .\unistaller
 
 :: Define the path to the text file, check it and get address
+setlocal enabledelayedexpansion
 set "file=release_repo"
 set "repo="
 set "searchKey=repo"
@@ -33,10 +32,33 @@ for /F "tokens=1,* delims==" %%A in (file) do (
 if %winRepo% == "" (
     echo Couldn't find any repos! Check release_repo file . . .
 )
-
 endlocal
 
+:: Trying to push binaries
+git remote | findstr /R /C:"^origin$" >nul
+if %ERRORLEVEL% neq 0 (
+    echo No remote repository found. Trying to add it . . .
+    git remote add origin %repo% >nul
+    if %errorlevel% neq 0 (
+        echo Can't add remote repository! Shutting down . . .
+        exit /b 1
+    )
+) 
+git fetch origin
 
+git branch --list win_release >nul
+if %ERRORLEVEL% neq 0 (
+    git checkout -b win_release origin/win_release
+) else (
+    git checkout win_release
+)
+
+git add .
+git commit -m "New windows release"
+git push --set-upstream origin win_release >nul 
+if %errorlevel% neq 0 (
+    echo Not being able to push to remote branch!
+)
 
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: 2 - build for linux and push to release (done inside docker file)
