1bc82bb52fa78173b9adebb93adc36cb29607fa8
TommaruX3700 <tommasomaruzzo@gmail.com>
2025-01-24 15:06:08 +0100

    Fix directory structure in deb_build_all.sh for release builds

--- deb_build_all.sh
+++ deb_build_all.sh
@@ -1,20 +1,20 @@
 DATE_=$(date +%d_%m_%Y)
 
-mkdir ./releases/deb_$DATE_
-mkdir ./releases/deb_$DATE_/installer
-mkdir ./releases/deb_$DATE_/server_watchdog
-mkdir ./releases/deb_$DATE_/uninstaller
+mkdir ./releases/deb/deb_$DATE_
+mkdir ./releases/deb/deb_$DATE_/installer
+mkdir ./releases/deb/deb_$DATE_/server_watchdog
+mkdir ./releases/deb/deb_$DATE_/uninstaller
 
 cd ./installer
 cargo build --release
-mv ./target/*  ../releases/deb_$DATE_/installer
+mv ./target/*  ../releases/deb/deb_$DATE_/installer
 cd ..
 cd ./server_watchdog
 cargo build --release
-mv ./target/*  ../releases/deb_$DATE_/server_watchdog
+mv ./target/*  ../releases/deb/deb_$DATE_/server_watchdog
 cd ..
 cd ./uninstaller
 cargo build --release
-mv ./target/*  ../releases/deb_$DATE_/uninstaller
+mv ./target/*  ../releases/deb/deb_$DATE_/uninstaller
 cd ..
 
