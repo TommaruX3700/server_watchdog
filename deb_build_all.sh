50c963a2faf9d9887c36edc87346220515459aae
TommaruX3700 <tommasomaruzzo@gmail.com>
2025-01-24 13:10:29 +0100

    Organize build outputs into separate directories for installer, server_watchdog, and uninstaller in both Debian and Windows build scripts

--- deb_build_all.sh
+++ deb_build_all.sh
@@ -1,16 +1,20 @@
 DATE_ = $date +%d_%m_%Y
+
 mkdir ./releases/deb_$DATE_
+mkdir ./releases/deb_$DATE_/installer
+mkdir ./releases/deb_$DATE_/server_watchdog
+mkdir ./releases/deb_$DATE_/uninstaller
 
 cd ./installer
 cargo build --release
-mv ./target/*  ../releases/deb_$DATE_
+mv ./target/*  ../releases/deb_$DATE_/installer
 cd ..
 cd ./server_watchdog
 cargo build --release
-mv ./target/*  ../releases/deb_$DATE_
+mv ./target/*  ../releases/deb_$DATE_/server_watchdog
 cd ..
-cd ./uninstaller/
+cd ./uninstaller
 cargo build --release
-mv ./target/*  ../releases/deb_$DATE_
+mv ./target/*  ../releases/deb_$DATE_/uninstaller
 cd ..
 
--- win_build_all.bat
+++ win_build_all.bat
@@ -3,20 +3,25 @@ for /F "tokens=2 delims==" %%I in ('"wmic os get localdatetime /value | findstr 
 set DATE_=%datetime:~6,2%_%datetime:~4,2%_%datetime:~0,4%
 
 mkdir releases\win\win_%DATE_%
+mkdir releases\win\win_%DATE_%\installer
+mkdir releases\win\win_%DATE_%\server_watchdog
+mkdir releases\win\win_%DATE_%\uninstaller
 
 cd installer
 cargo build --release
-move /Y target\* ..\releases\deb_%DATE_%\
+mkdir 
+move /Y target\* ..\releases\win\win_%DATE_%\installer
 cd ..
 
 cd server_watchdog
 cargo build --release
-move /Y target\* ..\releases\deb_%DATE_%\
+move /Y target\* ..\releases\win\win_%DATE_%\server_watchdog
 cd ..
 
 cd uninstaller
 cargo build --release
-move /Y target\* ..\releases\deb_%DATE_%\
+move /Y target\* ..\releases\win\win_%DATE_%\uninstaller
+
 cd ..
 
 @echo Build and file organization completed.
