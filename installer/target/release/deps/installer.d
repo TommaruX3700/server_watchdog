1987bb0466e1bfea8b1333003b731621b6f28f97
TommaruX3700 <tommasomaruzzo@gmail.com>
2025-01-24 13:05:15 +0100

    Remove obsolete installer build artifacts and dependency files

--- installer/target/release/deps/installer.d
+++
@@ -1,5 +0,0 @@
-C:\Users\TomX37\Documents\REPOS\server_watchdog\installer\target\release\deps\installer.exe: src/main.rs
-
-C:\Users\TomX37\Documents\REPOS\server_watchdog\installer\target\release\deps\installer.d: src/main.rs
-
-src/main.rs:
--- installer/target/release/installer.d
+++
@@ -1,1 +0,0 @@
-C:\Users\TomX37\Documents\REPOS\server_watchdog\installer\target\release\installer.exe: C:\Users\TomX37\Documents\REPOS\server_watchdog\installer\src\main.rs
